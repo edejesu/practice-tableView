0235e37b933c15fe442c0580bcf89c10166db581
edejesu <student@edejesu.local>
2016-01-15 10:11:11 -0600

    add/ delete names

--- table view practice/ViewController.swift
+++ table view practice/ViewController.swift
@@ -14,13 +14,45 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var myTableView: UITableView!
     var superheros = ["batman", "superman", "aquaman", "wonderwoman", "the flash", "the dirty bubble"]
     var realNames = ["brunce wayne", "clark kent", "authur curry", "dianna", "barry allen", "brown bubble"]
-    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // set tablesource data source & delegate to view controller
         myTableView.dataSource = self
         myTableView.delegate = self
+    }
+    //allows you to add a new superhero to tableview
+    @IBAction func addButtonTapped(sender: UIBarButtonItem)
+    {
+        let myAlert = UIAlertController(title: "add superhero", message: nil, preferredStyle: .Alert)
+        //way to attach an action to the alert
+        // makes spaces to tpye w placeholder text
+        myAlert.addTextFieldWithConfigurationHandler
+        {
+        (nameTextfield) -> Void in
+            nameTextfield.placeholder = "add superhero name"
+        }
+        myAlert.addTextFieldWithConfigurationHandler
+        {
+        (aliasTextfield) -> Void in
+        aliasTextfield.placeholder = "add alias superhero name"
+        }
+        //adds cancel button
+    var cancelAction = UIAlertAction(title: "cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        myAlert.addAction(cancelAction)
+        //add action to alert
+    var addAction = UIAlertAction(title: "add", style: .Default)
+        { (addAction) -> Void in
+            let superheroTF = myAlert.textFields![0] as UITextField
+            let aliasTF = myAlert.textFields![1] as UITextField
+            self.superheros.append(superheroTF.text!)
+            self.realNames.append(aliasTF.text!)
+            self.myTableView.reloadData()
+        }
+        myAlert.addAction(addAction)
+
+    //presents alert view
+        self.presentViewController (myAlert, animated: true, completion: nil)
     }
     //required for tableivew protocall - sends data to cell
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
@@ -38,6 +70,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     {
         return superheros.count
     }
-
+    //removing an item from the tableview/ array
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == .Delete
+        {
+            superheros.removeAtIndex(indexPath.row)
+            //removes from first array
+            realNames.removeAtIndex(indexPath.row)
+            //removes from second array
+            myTableView.reloadData()
+            //reloads everything
+        }
+    }
 }
 
